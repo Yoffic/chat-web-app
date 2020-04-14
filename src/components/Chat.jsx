55ca24147a46c15bedf0ce302afbf8904ee101b2
Olga Ioffe <yoffic@yahoo.com>
2020-04-14 19:33:00 +0300

    add feature display chat messages

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import ChatInput from './ChatInput.jsx';
 
+const Chat = () => {
+  const messages = useSelector((state) => state.messages);
+  const activeChannelId = useSelector((state) => state.activeChannelId);
 
-const Chat = () => (
-  <section className="col-9 d-flex px-4">
-    {/* depends on channels buttons */}
-    <section>Messages from participants</section>
-    {/* looks the same */}
-    <section className="mt-auto">
-      Input message form
+  return (
+    <section className="col-9 d-flex flex-column px-4">
+      <section>
+        <div>
+          {messages.map(({
+            id,
+            username,
+            message,
+            channelId,
+          }) => {
+            if (channelId !== activeChannelId) {
+              return null;
+            }
+            return (
+              <div key={id}>
+                <span className="mr-1 font-weight-bold">
+                  {username}
+                  :
+                </span>
+                <span>{message}</span>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <section className="mt-auto w-100">
+        <ChatInput />
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default Chat;
